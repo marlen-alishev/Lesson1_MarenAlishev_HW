cef24255bf546eff8f7c50bdc781a324bfd8252a
marlen-alishev <marlen.alishev@gmail.com>
2023-07-18 19:05:21 +0600

    The second task is done

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+int[] array1 = new int[3];
+Console.Write("Please insert the first number: ");
+array1[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please insert the second number: ");
+array1[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please insert the third number: ");
+array1[2] = Convert.ToInt32(Console.ReadLine());
+int max = 0; 
+int i = 0;  
+
+while (i < array1.Length) {
+    if (array1[i]>max)
+    {
+        max = array1[i];
+    }
+    i = i + 1;
+}
+Console.Write("Max number is " + max);
