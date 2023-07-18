27ab899a6676905f563bb37f2c6a11d4a18d196e
marlen-alishev <marlen.alishev@gmail.com>
2023-07-18 19:05:47 +0600

    The third task is done

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.Write("Please insert your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0){
+    Console.Write("Yes, it is an even number");
+}
+else 
+{
+    Console.Write("No, it is not an even number"); 
+}
