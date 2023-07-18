f8e2cb1f9b5bd1e360873237fdd19dac9a8b768a
marlen-alishev <marlen.alishev@gmail.com>
2023-07-18 19:06:15 +0600

    The fourth taks is done

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Please insert your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2; 
+
+Console.Write("All even numbers until N: ");
+while (i<=N) 
+{
+    Console.Write(i + " "); 
+    i = i + 2;
+}
+
